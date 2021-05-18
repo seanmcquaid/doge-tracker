0beb1ae9d97090764dee6b708011cef28dab85c8
seanmcquaid <sean.mcquaid2@verizon.net>
2021-05-17 22:56:08 -0400

    added in base query

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div></div>;
-}
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+};
 
 export default App;
--- src/queries/index.js
+++ src/queries/index.js
@@ -1,1 +1,19 @@
-export const getDogeInfo = () => {};
+import axios from 'axios';
+
+const key = process.env.REACT_APP_API_KEY || '';
+
+export const getDogeInfo = async () => {
+  const response = await axios.get(
+    'https://api.nomics.com/v1/currencies/ticker',
+    {
+      params: key,
+      ids: 'BTC,ETH,XRP',
+      interval: '1d,30d',
+      convert: 'EUR',
+      'per-page': '100',
+      page: '1',
+    }
+  );
+
+  return response?.data;
+};
